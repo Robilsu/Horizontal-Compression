ce2b344f9a6bd8fc9c87e985eae5ec597c721888
Robilsu <Robilsu@users.noreply.github.com>
2022-06-20 15:43:56 -0300

    Update !HaltingProof.lean
    Proof that "NEW Rule: ⊇-Elimination = ⊇-Elimination" results in a Type-1 neighborhood.

--- !HaltingProof.lean
+++ !HaltingProof.lean
@@ -26,7 +26,7 @@ inductive node
          (LABEL : ℕ)
          (FORMULA : formula)
 export node (vertex)
-notation LEVEL :: LABEL :: FORMULA := node.vertex LEVEL LABEL FORMULA
+notation LEVEL && LABEL && FORMULA := node.vertex LEVEL LABEL FORMULA
 /- Instances of Decidability (Equality): DLDS-Node -/
 instance node.decidable_eq :
   ∀(n1 n2 : node),
@@ -44,6 +44,13 @@ instance node.decidable_eq :
                       (@nat.decidable_eq LVL_1 LVL_2)
                       (decidable_and)
 end
+/- GET-Methods -/
+def node.get_level : node → ℕ
+| (vertex LEVEL LABEL FORMULA):= LEVEL
+def node.get_label : node → ℕ
+| (vertex LEVEL LABEL FORMULA):= LABEL
+def node.get_formula : node → formula
+| (vertex LEVEL LABEL FORMULA):= FORMULA
 
 /- Inductive Types: Dag-Like Derivability Structure -/
 inductive deduction
@@ -112,66 +119,104 @@ def list_deduction_paint : list deduction → ℕ → list deduction
 /- Colapso Simples (sem Arestas de Ancestralidade): -/
 /- Neighborhood: ⊇-Elimination -/
 def case_neighborhood_01 : neighborhood → Prop
-| (dag CTR IN OUT ANC) := ( ∃(antecedent consequent : formula),
-                            ∃(level center minor major : ℕ),
-                            ∃(dep_minor dep_major : set formula),
-                            ∃(out : node),
-                          ------------------------------------------------------
-                            CTR = vertex level center consequent
-                          ∧ IN = [ arrow (vertex level minor antecedent) CTR [] dep_minor,
-                                   arrow (vertex level major (antecedent>>consequent)) CTR [] dep_major ]
-                          ∧ OUT = [(arrow CTR out [] (dep_minor ∪ dep_major))]
-                          ∧ ANC = [] )
+| (dag (vertex LVL LBL FML) IN OUT ANC) := ( ∃(antecedent out_form : formula),
+                                             ∃(minor out_lbl : ℕ),
+                                             ∃(dep_minor dep_major : set formula),
+                                             ------------------------------------------------------
+                                             IN = [ arrow (vertex (LVL+1) minor antecedent)
+                                                          (vertex LVL LBL FML)
+                                                          []
+                                                          dep_minor,
+                                                    arrow (vertex (LVL+1) (minor+1) (antecedent>>FML))
+                                                          (vertex LVL LBL FML)
+                                                          []
+                                                          dep_major ]
+                                           ∧ OUT = [ arrow (vertex LVL LBL FML)
+                                                           (vertex (LVL-1) out_lbl out_form)
+                                                           []
+                                                           (dep_minor ∪ dep_major) ]
+                                           ∧ ANC = [] )
 /- Neighborhood: ⊇-Introduction -/
 def case_neighborhood_02 : neighborhood → Prop
 | (dag CTR IN OUT ANC) := ( ∃(antecedent consequent : formula),
-                            ∃(level center intro : ℕ),
+                            ∃(level label intro : ℕ),
                             ∃(dep : set formula),
                             ∃(out : node),
                           ------------------------------------------------------
-                            CTR = vertex level center (antecedent>>consequent)
-                          ∧ IN = [arrow (vertex level intro antecedent) CTR [] {x | x = antecedent ∨ x ∈ dep}]
+                            CTR = vertex level label (antecedent>>consequent)
+                          ∧ IN = [arrow (vertex (level+1) intro antecedent) CTR [] {x | x = antecedent ∨ x ∈ dep}]
                           ∧ OUT = [arrow CTR out [] {x | x ≠ antecedent ∧ x ∈ dep}]
                           ∧ ANC = [] )
 /- Neighborhood: Hypothesis -/
 def case_neighborhood_03 : neighborhood → Prop
 | (dag CTR IN OUT ANC) := ( ∃(hypothesis : formula),
-                            ∃(level center : ℕ),
+                            ∃(level label : ℕ),
                             ∃(out : node),
                           ------------------------------------------------------
-                            CTR = vertex level center hypothesis
-                          ∧ IN = []
-                          ∧ OUT = [arrow CTR out [] {hypothesis}]
-                          ∧ ANC = [] )
-/- Neighborhood: Collapse -/
-def case_neighborhood_04 : neighborhood → Prop
-| (dag CTR IN OUT ANC) := ( ∃(hypothesis : formula),
-                            ∃(level center : ℕ),
-                            ∃(out : node),
-                          ------------------------------------------------------
-                            CTR = vertex level center hypothesis
+                            CTR = vertex level label hypothesis
                           ∧ IN = []
                           ∧ OUT = [arrow CTR out [] {hypothesis}]
                           ∧ ANC = [] )
 /- Pre-Colapso Simples (sem Arestas de Ancestralidade): -/
-def node.get_level : node → ℕ
-| (vertex LEVEL LABEL FORMULA):= LEVEL
-def node.get_label : node → ℕ
-| (vertex LEVEL LABEL FORMULA):= LABEL
-def loop_create_ancestral : deduction → deduction → ancestral
+def loop_simple_ancestral : ℕ → deduction → deduction → ancestral
+| COLOUR
+  (arrow IN_STR IN_END IN_CLR IN_DEP)
+  (arrow OUT_STR OUT_END OUT_CLR OUT_DEP) := path OUT_END
+                                                  IN_STR
+                                                  [COLOUR]
+def create_simple_ancestral : ℕ → list deduction → list deduction → list ancestral
+| COLOUR (HEAD::TAIL) [OUT] := (loop_simple_ancestral COLOUR HEAD OUT)::(create_simple_ancestral COLOUR TAIL [OUT])
+| COLOUR IN OUT := []
+/- Neighborhood: Collapse -/
+def simple_pre_collapse : neighborhood → neighborhood
+| (dag (vertex LVL LBL FML) IN OUT ANC) := dag (vertex LVL LBL FML)
+                                               IN
+                                               (list_deduction_paint OUT LBL)
+                                               (create_simple_ancestral LBL IN OUT)
+def simple_horizontal_collapse : neighborhood → neighborhood → neighborhood
+| (dag (vertex LVL_U LBL_U FML_U) IN_U OUT_U ANC_U)
+  (dag (vertex LVL_V LBL_V FML_V) IN_V OUT_V ANC_V) := dag (vertex LVL_U LBL_U FML_U)
+                                                           (   IN_U
+                                                            ++ list_deduction_rewrite IN_V
+                                                                                      (vertex LVL_V LBL_V FML_V)
+                                                                                      (vertex LVL_U LBL_U FML_U)   )
+                                                           (   (list_deduction_paint OUT_U LBL_U)
+                                                            ++ list_deduction_rewrite (list_deduction_paint OUT_V LBL_V)
+                                                                                      (vertex LVL_V LBL_V FML_V)
+                                                                                      (vertex LVL_U LBL_U FML_U)   )
+                                                           (   (create_simple_ancestral LBL_U IN_U OUT_U)
+                                                            ++ list_ancestral_rewrite (create_simple_ancestral LBL_V IN_V OUT_V)
+                                                                                      (vertex LVL_V LBL_V FML_V)
+                                                                                      (vertex LVL_U LBL_U FML_U)   )
+                                                                                      
+/- Pre-Colapso Comum (sem Arestas de Ancestralidade): -/
+def loop_loop_ancestral : deduction → deduction → ancestral
 | (arrow IN_STR IN_END IN_CLR IN_DEP)
   (arrow OUT_STR OUT_END OUT_CLR OUT_DEP) := path OUT_END
                                                   IN_STR
-                                                  [node.get_label OUT_END]
-def simple_create_ancestral : list deduction → list deduction → list ancestral
-| (HEAD::TAIL) [OUT] := (loop_create_ancestral HEAD OUT)::(simple_create_ancestral TAIL [OUT])
-| IN OUT := []
-def simple_pre_collapse : neighborhood → neighborhood
-| (dag (vertex LVL LBL FML) IN OUT ANC) := dag (vertex LVL LBL FML)
-                                           IN
-                                           (list_deduction_paint OUT LVL)
-                                           (simple_create_ancestral IN OUT)
+                                                  OUT_CLR --AQUI!!!
+def loop_ancestral : deduction → list deduction → list ancestral
+| IN [] := []
+| IN (HEAD::TAIL) := (loop_loop_ancestral IN HEAD)::(loop_ancestral IN TAIL)
+def create_ancestral : list deduction → list deduction → list ancestral
+| [] OUT := []
+| (HEAD::TAIL) OUT := (loop_ancestral HEAD OUT) ++ (create_ancestral TAIL OUT)
+/- Neighborhood: Type-1 Halfs -/
+def case_neighborhood_04 : neighborhood → Prop
+| (dag (vertex LVL LBL FML) IN OUT ANC) := ( IN = IN --AQUI!!!
+                                           ∧ OUT = OUT --AQUI!!!
+                                           ∧ list.length ANC = list.length IN
+                                           ∧ ∀(a : ancestral), a ∈ ANC → a ∈ create_ancestral IN OUT )
+                                           --∧ ANC = create_ancestral IN OUT )
 
+/- Conditions for Collapse -/
+def check_nodes : neighborhood → neighborhood → Prop
+| (dag (vertex LVL_U LBL_U FML_U) IN_U OUT_U ANC_U)
+  (dag (vertex LVL_V LBL_V FML_V) IN_V OUT_V ANC_V) := (LVL_U = LVL_V)
+                                                     ∧ (LVL_U > 0)
+                                                     ∧ (LVL_V > 0)
+                                                     ∧ (LBL_U ≠ LBL_V)
+                                                     ∧ (FML_U = FML_V)
 /- NEW Rule: ⊇-Elimination = ⊇-Elimination -/
 def case_01_01 : neighborhood → neighborhood → Prop
 | U V := ( case_neighborhood_01 U )
@@ -208,3 +253,158 @@ def case_03_03 : neighborhood → neighborhood → Prop
        ∧ ( case_neighborhood_03 V )
 def case_03_03S : neighborhood → neighborhood → Prop
 | L R := case_03_03 R L
+
+/- Proof -/
+lemma Lemma_Add :
+  ∀{n : ℕ},
+  ---------------------------
+  ¬( n+1 = n )
+| 0 := by simp
+| (n+1) := begin
+  rewrite[←nat.succ_eq_add_one (n+1)],
+  rewrite[←nat.succ_eq_add_one n],
+  simp at ⊢,
+  from Lemma_Add
+end
+
+lemma Lemma_Sub :
+  ∀{n : ℕ},
+  ( n > 0 ) →
+  ---------------------------
+  ¬( n-1 = n )
+| 0 := begin
+  assume n_gt_zero,
+  have not_n_gt_zero, from gt_irrefl 0,
+  from false.elim (not_n_gt_zero n_gt_zero)
+end
+| (n+1) := begin
+  assume n_gt_zero,
+  rewrite[←nat.succ_eq_add_one n],
+  simp at ⊢,
+  have n_lt_succ, from nat.lt_succ_of_le (le_refl n),
+  from ne_of_lt n_lt_succ
+end
+
+lemma Main_Lemma_01_01 :
+  ∀(u v : neighborhood),
+  ( check_nodes u v ) →
+  ( case_01_01 u v ) →
+  ---------------------------
+  ( case_neighborhood_04 (simple_horizontal_collapse u v) )
+| (dag (vertex LVL_U LBL_U FML_U) IN_U OUT_U ANC_U)
+  (dag (vertex LVL_V LBL_V FML_V) IN_V OUT_V ANC_V) := begin
+  assume prob_nodes prob_01_01,
+
+  simp [check_nodes] at prob_nodes,
+  cases prob_nodes with prob_eq_lvl prob_nodes,
+  cases prob_nodes with prob_lvl_u prob_nodes,
+  cases prob_nodes with prob_lvl_v prob_nodes,
+  cases prob_nodes with prob_ne_lbl prob_eq_fml,
+
+  simp [case_01_01] at prob_01_01,
+  cases prob_01_01 with prob_u_01 prob_v_01,
+  simp [simple_horizontal_collapse],
+
+  simp [case_neighborhood_01] at prob_v_01,
+  cases prob_v_01 with antecedent_v prob_v_01,
+  cases prob_v_01 with out_form_v prob_v_01,
+  cases prob_v_01 with minor_v prob_v_01,
+  cases prob_v_01 with out_lbl_v prob_v_01,
+  cases prob_v_01 with dep_minor_v prob_v_01,
+  cases prob_v_01 with dep_major_v prob_v_01,
+  cases prob_v_01 with prob_in_v prob_v_01,
+  cases prob_v_01 with prob_out_v prob_anc_v,
+
+  rewrite [prob_in_v],
+  simp [list_deduction_rewrite, deduction_rewrite],
+  simp [node_rewrite, Lemma_Add],
+
+  rewrite [prob_out_v],
+  simp [list_deduction_paint, deduction_paint, colour_paint],
+  simp [list_deduction_rewrite, deduction_rewrite],
+  simp [node_rewrite, Lemma_Sub prob_lvl_v],
+  
+  simp [create_simple_ancestral, loop_simple_ancestral],
+  simp [list_ancestral_rewrite, ancestral_rewrite],
+  simp [node_rewrite, Lemma_Add, Lemma_Sub prob_lvl_v],
+
+  simp [case_neighborhood_01] at prob_u_01,
+  cases prob_u_01 with antecedent_u prob_u_01,
+  cases prob_u_01 with out_form_u prob_u_01,
+  cases prob_u_01 with minor_u prob_u_01,
+  cases prob_u_01 with out_lbl_u prob_u_01,
+  cases prob_u_01 with dep_minor_u prob_u_01,
+  cases prob_u_01 with dep_major_u prob_u_01,
+  cases prob_u_01 with prob_in_u prob_u_01,
+  cases prob_u_01 with prob_out_u prob_anc_u,
+
+  rewrite [prob_in_u, prob_out_u],
+  simp [list_deduction_paint, deduction_paint, colour_paint],
+  simp [create_simple_ancestral, loop_simple_ancestral],
+  
+  simp [case_neighborhood_04],
+  simp [create_ancestral],
+  simp [loop_ancestral],
+  simp [loop_loop_ancestral],
+  
+  assume a a_cases,
+  cases a_cases with a_case_01 a_cases,
+  case or.inl { simp [a_case_01] },
+  cases a_cases with a_case_02 a_cases,
+  case or.inl { simp [a_case_02] },
+  cases a_cases with a_case_03 a_case_04,
+  case or.inl { simp [a_case_03] },
+  case or.inr { simp [a_case_04] }
+end
+
+/- Tests -/
+constants LVL_U LVL_V : ℕ
+--def LVL_U : ℕ := 10
+--def LVL_V : ℕ := 10
+constants LBL_U LBL_V : ℕ
+--def LBL_U : ℕ := 1
+--def LBL_V : ℕ := 2
+constants FML_U FML_V : formula
+--def FML_U : formula := #1
+--def FML_V : formula := #2
+constants minor_u out_lbl_u : ℕ
+--def minor_u : ℕ := 11
+--def out_lbl_u : ℕ := 13
+constants antecedent_u out_form_u : formula
+--def antecedent_u : formula := #11
+--def out_form_u : formula := #13
+constants dep_minor_u dep_major_u : set formula
+--def dep_minor_u : set formula := ∅
+--def dep_major_u : set formula := ∅
+constants minor_v out_lbl_v : ℕ
+--def minor_v : ℕ := 21
+--def out_lbl_v : ℕ := 23
+constants antecedent_v out_form_v : formula
+--def antecedent_v : formula := #21
+--def out_form_v : formula := #23
+constants dep_minor_v dep_major_v : set formula
+--def dep_minor_v : set formula := ∅
+--def dep_major_v : set formula := ∅
+
+--create_ancestral
+--  [ arrow ((LVL_U + 1)&&minor_u&&antecedent_u) (LVL_U&&LBL_U&&FML_U) [] dep_minor_u,
+--    arrow ((LVL_U + 1)&&(minor_u + 1)&&antecedent_u>>FML_U) (LVL_U&&LBL_U&&FML_U) [] dep_major_u,
+--    arrow ((LVL_V + 1)&&minor_v&&antecedent_v) (LVL_U&&LBL_U&&FML_U) [] dep_minor_v,
+--	arrow ((LVL_V + 1)&&(minor_v + 1)&&antecedent_v>>FML_V) (LVL_U&&LBL_U&&FML_U) [] dep_major_v ]
+--  [ arrow (LVL_U&&LBL_U&&FML_U) ((LVL_U - 1)&&out_lbl_u&&out_form_u) [LBL_U] (dep_minor_u ∪ dep_major_u),
+--    arrow (LVL_U&&LBL_U&&FML_U) ((LVL_V - 1)&&out_lbl_v&&out_form_v) [LBL_V] (dep_minor_v ∪ dep_major_v) ]
+--=
+--[ path (nat.pred LVL_U&&out_lbl_u&&out_form_u) (nat.succ LVL_U&&minor_u&&antecedent_u) [LBL_U],
+--  path (nat.pred LVL_V&&out_lbl_v&&out_form_v) (nat.succ LVL_U&&minor_u&&antecedent_u) [LBL_V],
+--  path (nat.pred LVL_U&&out_lbl_u&&out_form_u) (nat.succ LVL_U&&nat.succ minor_u&&antecedent_u>>FML_U) [LBL_U],
+--  path (nat.pred LVL_V&&out_lbl_v&&out_form_v) (nat.succ LVL_U&&nat.succ minor_u&&antecedent_u>>FML_U) [LBL_V],
+--  path (nat.pred LVL_U&&out_lbl_u&&out_form_u) (nat.succ LVL_V&&minor_v&&antecedent_v) [LBL_U],
+--  path (nat.pred LVL_V&&out_lbl_v&&out_form_v) (nat.succ LVL_V&&minor_v&&antecedent_v) [LBL_V],
+--  path (nat.pred LVL_U&&out_lbl_u&&out_form_u) (nat.succ LVL_V&&nat.succ minor_v&&antecedent_v>>FML_V) [LBL_U],
+--  path (nat.pred LVL_V&&out_lbl_v&&out_form_v) (nat.succ LVL_V&&nat.succ minor_v&&antecedent_v>>FML_V) [LBL_V] ]
+
+def ANC := create_ancestral  [ arrow ((LVL_U + 1)&&minor_u&&antecedent_u) (LVL_U&&LBL_U&&FML_U) [] dep_minor_u, arrow ((LVL_U + 1)&&(minor_u + 1)&&antecedent_u>>FML_U) (LVL_U&&LBL_U&&FML_U) [] dep_major_u, arrow ((LVL_V + 1)&&minor_v&&antecedent_v) (LVL_U&&LBL_U&&FML_U) [] dep_minor_v, arrow ((LVL_V + 1)&&(minor_v + 1)&&antecedent_v>>FML_V) (LVL_U&&LBL_U&&FML_U) [] dep_major_v ]  [ arrow (LVL_U&&LBL_U&&FML_U) ((LVL_U - 1)&&out_lbl_u&&out_form_u) [LBL_U] (dep_minor_u ∪ dep_major_u), arrow (LVL_U&&LBL_U&&FML_U) ((LVL_V - 1)&&out_lbl_v&&out_form_v) [LBL_V] (dep_minor_v ∪ dep_major_v) ]
+
+#check ANC
+#print ANC
+#reduce ANC
